1cfc105562459b290cc34b638d3d8c856d2b50b5
Ivan Paulovich <7133698+ivanpaulovich@users.noreply.github.com>
2024-01-12 16:50:37 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
 echo "** Installing git **";
-
 xcode-select --install
+fi
+
+echo "** Installing brew and its packages **";
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install $(cat packages.txt)
