8a40bd0b4ef0e5bb4eb450dbafa00e4f10609720
Ivan Paulovich <7133698+ivanpaulovich@users.noreply.github.com>
2024-01-12 20:39:02 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-echo "** Installing git **";
+echo "**** Installing git ****";
 xcode-select --install
 fi
 
-echo "** Installing brew and its packages **";
+echo "**** Installing brew and its packages ****";
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install $(cat packages.txt)
+
+echo "**** Installing ohmyzsh ****";
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "**** Installing powerlevel10k theme ****";
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "**** Installing COC ****";
+
+sh ./install-coc.sh
